c9d5bd658983d05d117594d7d0ddcd97d8eb2227
DAngelo Thomas <dangelo1thomas@gmail.com>
2021-07-12 18:04:35 -0400

    add errors partial and  validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
     has_secure_password
+    validates :username, presence: true
+    validates :email, presence: true
+    validates :email, uniqueness: true
+    validates :password, presence: true
+    validates :password, length: { in: 5..20 }
 
     has_many :shoes
     has_many :favorites

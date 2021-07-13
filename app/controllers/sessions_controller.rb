fe26fc37e20a63bebf86462d00f6709b73ae613f
DAngelo Thomas <dangelo1thomas@gmail.com>
2021-07-12 23:53:50 -0400

    index action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,22 +5,18 @@ class SessionsController < ApplicationController
     end
 
     def create
-        user = User.find_by_username(params[:username])
-        if user && user.authenticate(params[:password])
-            redirect_to user
+        @user = User.find_by(username: params[:username])
+        if @user && @user.authenticate(params[:password])
+        redirect_to @user
         else
             flash[:errors] = ["wrong username or password"]
             redirect_to login_path
-        # byebug
         end
     end
 
     def logout
         session.clear
         redirect_to new_user_path
-
     end
 
-    def login
-    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,17 @@
 class UsersController < ApplicationController
 
     def new
+        # render :layout => "signup"
         @user = User.new
     end
-
+    
+    def index
+        @users = User.all 
+    end
+    
+    
     def create 
+        
         @user = User.new(user_params)
 
         if @user.save

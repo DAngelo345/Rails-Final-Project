dddf887d848da977e17992b1e4732c1b31b54a01
DAngelo Thomas <dangelo1thomas@gmail.com>
2021-07-14 21:58:52 -0400

    fixed nav bar to have a profile option

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,45 @@
 class FavoritesController < ApplicationController
+    def index
+        @favorites = Favorite.all
+        # binding.pry
+       
+    end
+
+    def new
+        @favorite = Favorite.new 
+    end
+
+    def show
+        @favorite = Favorite.find(params[:id])
+    end
+
+    def create
+        @favorite = current_user.favorites.build(favorite_params)
+        
+        if @favorite.save
+            redirect_to @favorite
+        else
+            render :new
+        end
+    end
+    def edit
+        @favorite = Favorite.find(params[:id])
+    end
+
+    def update
+        @favorite = Favorite.find(params[:id])
+        @favorite.update(favorite_params)
+        redirect_to favorite_path(@shoe)
+    end
+
+    def destroy
+        Favorite.find(params[:id]).destroy
+        redirect_to user_path
+    end
+
+    private
+
+    def favorite_params
+        params.require(:favorite).permit(:favorite_count)
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,7 @@ class UsersController < ApplicationController
     def show
         redirect_if_not_logged_in
         @user = User.find_by_id(params[:id])
+        # binding.pry
         redirect_to '/login' if !@user
     end
 
